fa8b8213222e4d4aa7bd3c6648ac6a48171a54f3
David Florez Robles <81florezroblesdavidalejandro@gmail.com>
2026-08-10 12:20:04 -0500

    Primeras variables en Java y aprendizajes

---
+++ src/variables.java
@@ -0,0 +1,22 @@
+public class variables
+{
+    public static void main(String[] args)
+    {
+        String nombre = "David A.";
+        int edad = 19;
+        float dinero = 1200.34f;
+        double decimal = 3.1415; 
+        /*cualquier decimal en java es tomado como double a menos
+          se le coloque la letre f a una declaracion float
+        */
+        char caracter = 'b';
+        boolean booleano = true;
+        boolean boolenao2 = false;
+        System.out.println("Mi nombre es " + nombre);
+        System.out.println("Mi edad es " + edad );
+        System.out.println("Mi dinero es " + dinero  );
+        System.out.println("Mi caracter es " + caracter);
+        System.out.println ("Booleanos " + booleano + " El otro " + boolenao2);
+
+    }    
+}
