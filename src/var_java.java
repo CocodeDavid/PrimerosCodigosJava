e70971f2481d1af80a98d981495e4d8a126424a0
David Florez Robles <81florezroblesdavidalejandro@gmail.com>
2026-08-10 23:22:50 -0500

    Var para Java y simplificacion de declaracion de variables

---
+++ src/var_java.java
@@ -0,0 +1,9 @@
+public class var_java
+{
+    public static void main(String [] args)
+    {
+        var nombre = "Mi nombre";
+        // var edad; esto no es posible siempre se tiene que inicializar
+        System.out.println(nombre + " hola a todos");
+    }
+}
