e8be6eb9decf69fac2bb586e8d594550ecccd05b
David Florez Robles <81florezroblesdavidalejandro@gmail.com>
2026-08-16 15:44:01 -0500

    Variables constantes en Java

---
+++ src/constantes.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class constantes 
+{
+    public static void main(String [] args)
+    {
+        final int valor = 50;
+        //valor = 200; //no se puede saldria error 
+        JOptionPane.showMessageDialog(null, valor);
+        final int valor2;
+        valor2= 200;
+        JOptionPane.showMessageDialog(null, valor2);
+    }    
+}
